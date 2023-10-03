4eddc12947c8af32d75066891f342b57263d9cbd
ihor-chernyshev <144264214+ihor-chernyshev@users.noreply.github.com>
2023-10-03 12:26:18 -0400

    typescript version

--- main.ts
+++ main.ts
@@ -1,8 +1,20 @@
 /* Copyright (c) 2020 MTHS All rights reserved
  *
- * Created by: Mr. Coxall
- * Created on: Sep 2020
- * This program ...
+ * Created by: Ihor Chernyshev
+ * Created on: Oct 2023
+ * This program does "Cookie Clicer" on MicroBit
 */
 
-basic.showString('Hello, World!')
+basic.clearScreen()
+basic.showIcon(IconNames.Happy)
+let numbers = 0
+
+input.onButtonPressed(Button.A, function() {
+  numbers = numbers + 1
+  basic.showNumber(numbers)
+})
+
+input.onButtonPressed(Button.B, function() {
+    numbers = 0
+    basic.showNumber(numbers)
+})
