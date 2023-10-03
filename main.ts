d048575d0c66b8855ecf0373c2997593301fc8fa
ihor-chernyshev <144264214+ihor-chernyshev@users.noreply.github.com>
2023-10-03 13:02:23 -0400

    typescript version

--- main.ts
+++ main.ts
@@ -9,12 +9,12 @@ basic.clearScreen()
 basic.showIcon(IconNames.Happy)
 let numbers = 0
 
-input.onButtonPressed(Button.A, function() {
+input.onButtonPressed(Button.A, function () {
   numbers = numbers + 1
   basic.showNumber(numbers)
 })
 
-input.onButtonPressed(Button.B, function() {
-    numbers = 0
-    basic.showNumber(numbers)
+input.onButtonPressed(Button.B, function () {
+  numbers = 0
+  basic.showNumber(numbers)
 })
