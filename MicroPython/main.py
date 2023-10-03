c104b5709b203729a797b833e8d59f15c282a701
ihor-chernyshev <144264214+ihor-chernyshev@users.noreply.github.com>
2023-10-03 17:23:21 +0000

    python version

--- MicroPython/main.py
+++ MicroPython/main.py
@@ -1,10 +1,19 @@
 """
-Created by: Mr. Coxall
-Created on: Sep 2020
+Created by: Ihor Chernyshev
+Created on: Oct 2023
 This module is a Micro:bit MicroPython program
 """
 
 from microbit import *
 
+display.clear()
+display.show(Image.HAPPY)
+numbers_of_cliks = 0
 
-display.scroll("Hello, World!")
+while True: 
+    if button_a.was_pressed(): 
+        numbers_of_cliks = numbers_of_cliks + 1
+        display.scroll(numbers_of_cliks)
+    if button_b.was_pressed():
+        numbers_of_cliks = 0
+        display.scroll(numbers_of_cliks)
